5cc10401a189e670cb1ac680bdc972272c8d2010
Dean William Weiss <dean.w.weiss@gmail.com>
2022-04-05 14:06:30 -0700

    added in console logs

--- js/app.js
+++ js/app.js
@@ -7,51 +7,66 @@ alert (`Welcome ${siteVisitor}, let's play a guessing game. Please answer yes or
 let questionOne = prompt ('Do I have any children?').toLowerCase();
 
 if(questionOne === 'no' || questionOne === 'n'){
+    //console.log(`You are correct`)
     alert('You are correct.');
 } else if(questionOne === 'yes' || questionOne === 'y'){
+    //console.log(`Sorry, that was a wrong guess.`)
     alert('Sorry, that was a wrong guess');
 } else{
-Alert('Please answer with a valid response!');
+    //console.log(`Please answer with a valid response!`)
+    Alert('Please answer with a valid response!');
 }
 
 let questionTwo = prompt ('Am I in the military?').toLowerCase();
 
 if(questionTwo === 'yes' || questionTwo === 'y'){
+    //console.log(`You are correct`)
     alert('You are correct.');
 } else if(questionTwo === 'no' || questionTwo === 'n'){
-    alert('Sorry, that was a wrong guess');
+    //console.log(`Sorry, that was a wrong guess.`)
+    alert('Sorry, that was a wrong guess.');
 } else{
-Alert('Please answer with a valid response!');
+    //console.log(`Please answer with a valid response!`)
+    Alert('Please answer with a valid response!');
 }
 
 let questionThree = prompt ('Is my favorite football team is the Seattle Seahawks?').toLowerCase();
 
 if(questionThree === 'no' || questionThree === 'n'){
+    //console.log(`You are correct`)
     alert('You are correct.');
 } else if(questionThree === 'yes' || questionThree === 'y'){
+    //console.log(`Sorry, that was a wrong guess.`)
     alert('Sorry, that was a wrong guess');
 } else{
-Alert('Please answer with a valid response!');
+    //console.log(`Please answer with a valid response!`)
+    Alert('Please answer with a valid response!');
 }
 
 let questionFour = prompt ('Do I have a dog?').toLowerCase();
 
 if(questionFour === 'yes' || questionFour === 'y'){
+    //console.log(`You are correct`)
     alert('You are correct.');
 } else if(questionFour === 'no' || questionFour === 'n'){
+    //console.log(`Sorry, that was a wrong guess.`)
     alert('Sorry, that was a wrong guess');
 } else{
-Alert('Please answer with a valid response!');
+    //console.log(`Please answer with a valid response!`)
+    Alert('Please answer with a valid response!');
 }
 
 let questionFive = prompt ('Have I ever broken a bone in my body?').toLowerCase();
 
 if(questionFive === 'no' || questionFive === 'n'){
+    //console.log(`You are correct`)
     alert('You are correct.');
 } else if(questionFive === 'yes' || questionFive === 'y'){
+    //console.log(`Sorry, that was a wrong guess.`)
     alert('Sorry, that was a wrong guess');
-} else{
-Alert('Please answer with a valid response!');
+} else{ 
+    //console.log(`Please answer with a valid response!`)
+    Alert('Please answer with a valid response!');
 }
 
 alert (`${siteVisitor}, thank you for visiting my website, I hope to see you again`);
